293ada7f3cf698e756b483ac482f55a930df53bc
RajaPidikitiiOS <rajasekhar.ppp@outlook.com>
2025-11-16 18:01:58 +0530

    First Commit

---
+++ Networking_Framework_Cocoapod/Networking_Framework_Cocoapod/Networking_Framework_Cocoapod.swift
@@ -0,0 +1,16 @@
+//
+//  Networking_Framework_Cocoapod.swift
+//  Networking_Framework_Cocoapod
+//
+//  Created by rajasekhar pidikiti on 16/11/25.
+//
+
+import Foundation
+
+public class Networking_Framework_Cocoapod {
+    public init() {}
+
+    public func hello() -> String {
+        return "Hello from Networking_Framework_Cocoapod!"
+    }
+}
---
+++ Networking_Framework_Cocoapod/Networking_Framework_CocoapodTests/Networking_Framework_CocoapodTests.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_Framework_CocoapodTests.swift
+//  Networking_Framework_CocoapodTests
+//
+//  Created by rajasekhar pidikiti on 16/11/25.
+//
+
+import Testing
+@testable import Networking_Framework_Cocoapod
+
+struct Networking_Framework_CocoapodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
